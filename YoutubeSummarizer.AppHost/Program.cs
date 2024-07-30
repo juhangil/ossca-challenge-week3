b94242e56514c77d82f36ddca0f7352ad157afbe
juhangil <tensil94@gmail.com>
2024-07-30 11:17:53 +0900

    [Add] Blazor web app and Asp core api app

--- YoutubeSummarizer.AppHost/Program.cs
+++ YoutubeSummarizer.AppHost/Program.cs
@@ -6,4 +6,13 @@ var config = builder.Configuration;
 
 Console.WriteLine($"\n Aspire Start, Launch Type: {config["LaunchType"]}\n");
 
+var backend = builder.AddProject<Projects.YoutubeSummarizer_Backend>("backend")
+    .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
+    .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
+    .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
+
+builder.AddProject<Projects.YoutubeSummarizer_Frontend>("frontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(backend);
+
 builder.Build().Run();
