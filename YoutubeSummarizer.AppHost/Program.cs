f75ddf3f3d94acf14bd7ea95acc6baf99a0754fb
juhangil <tensil94@gmail.com>
2024-07-30 10:58:09 +0900

    [Edit] print launch type on app host startup

--- YoutubeSummarizer.AppHost/Program.cs
+++ YoutubeSummarizer.AppHost/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var config = builder.Configuration;
+
+Console.WriteLine($"\n Aspire Start, Launch Type: {config["LaunchType"]}\n");
 
 builder.Build().Run();
